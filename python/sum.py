647f0b1aceb4a2641f62ac6b7eb597bf524b6f64
Wesley Osogo <wesleyosogo@college.harvard.edu>
2021-10-03 16:36:11 -0400

    python submission

--- python/sum.py
+++ python/sum.py
@@ -1,7 +1,16 @@
 def sum(l, N):
     # Write your code here!
-    
+    # make set
+    # traverse list checking if N - l[i] is in set
+    # if not add number to set
+
+    set_list = set()
+    for number in l:
+        if (N- number) in set_list:
+            return True
+        set_list.add(number)
     return False
+
 
 def test():
     assert sum([-1, 1], 0)
